44bc700623dbdb67eda064283b43096b99f0fb09
JoseGerardo <JoseGerardo@10.31.44.227>
2017-11-15 18:07:47 -0400

    made the jFrame appear in the middle of the screen

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,18 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
-	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
-		myFrame.setVisible(true);
-	}
+    public static void main(String[] args) {
+        JFrame myFrame = new JFrame("Jose Beauchamp");
+        myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        int width = (int) screenSize.getWidth(); // the screen width
+        int height = (int) screenSize.getHeight();// the screen height
+        myFrame.setLocation((width/2) -100, (height/2) -100);
+        myFrame.setSize(200, 200);
+        myFrame.setVisible(true);
+    }
 }
